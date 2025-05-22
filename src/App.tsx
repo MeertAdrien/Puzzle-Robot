e54a20590e823cf3b2090f7eccbe19f1078b97dd
Adrien Meert <adrien.meert92@gmail.com>
2025-05-22 15:50:57 +0200

    dev branch creat

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import "./App.css";
+import PuzzleRobot from './components/Robot-Puzzle';
 
 
 function App (){
@@ -7,6 +8,7 @@ function App (){
     <>
       <div>
         <Header />  
+        <PuzzleRobot/>
       </div>
       
     </>
---
+++ src/components/Robot-Puzzle.tsx
@@ -0,0 +1,22 @@
+import styles from "./Robot-Puzzle.module.css"
+
+
+export const PuzzleRobot = () => {
+    return (
+<section className={styles.section}>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 1.png" alt="Robot H/G"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 2.png" alt="Robot H/M"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 3.png" alt="Robot H/D"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 4.png" alt="Robot M/G"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 5.png" alt="Robot M/M"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 6.png" alt="Robot M/D"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 7.png" alt="Robot B/G"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 8.png" alt="Robot B/M"/></div>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 9.png" alt="Robot B/D"/></div>
+ 
+</section>
+    );
+};
+export default PuzzleRobot;
+
+   
