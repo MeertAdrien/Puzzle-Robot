c117bd36811f8c5ad3fc0b8248656b7083b9789b
Adrien Meert <adrien.meert92@gmail.com>
2025-05-22 14:46:30 +0200

    firts commit

---
+++ src/component/Robot-Puzzle.tsx
@@ -0,0 +1,23 @@
+
+import styles from "./Header.module.css"
+
+
+export const PuzzleRobot = () => {
+    return (
+<section className={styles.section}>
+  <div><img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc 2.png" alt="Robot H/G"/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt="Robot H/M"/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt="Robot H/M"/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt=""/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt=""/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt=""/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt=""/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt=""/></div>
+  <div><img src="src/assets/un_robot_entrain _de_faire_du_code_sur_un_pc 1.png" alt=""/></div>
+ 
+</section>
+    );
+};
+export default PuzzleRobot;
+
+   
---
+++ src/component/header.tsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css"
+
+export const Header = () => {
+    return (
+    <header>
+         <h1> Puzzle Robot </h1>
+        <img src="src/assets/un_robot_entrain_de_faire_du_code_sur_un_pc.png" alt = "Robot" />
+    
+    </header>
+    );
+};
+export default Header;
